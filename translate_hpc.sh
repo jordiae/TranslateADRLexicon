af6b7b97180dabc907ca244503ba175f6f2ab4eb
Jordi Armengol Estapé <me@jordiae.com>
2019-12-23 17:11:54 +0100

    HPC translate script

--- translate_hpc.sh
+++ translate_hpc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=pubmed_translate2
-#SBATCH --workdir=/gpfs/scratch/bsc88/bsc88251/PubMedTranslate
-#SBATCH --output=/gpfs/scratch/bsc88/bsc88251/PubMedTranslate/translate2.log
-#SBATCH --error=/gpfs/scratch/bsc88/bsc88251/PubMedTranslate/translate2.err
+#SBATCH --job-name=TranslateADRLexicon
+#SBATCH --workdir=/gpfs/home/bsc88/bsc88251/TranslateADRLexicon
+#SBATCH --output=/gpfs/home/bsc88/bsc88251/TranslateADRLexicon/translate.log
+#SBATCH --error=/gpfs/home/bsc88/bsc88251/TranslateADRLexicon/translate.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
 #SBATCH --time=48:00:00
@@ -15,11 +15,11 @@ module load python/3.6.5_ML
 
 export omp_num_threads=40
 
-bash tokenize_SP.sh -d /gpfs/projects/bsc88/_old/Resources/NMT_WMT19 -s en -t es -f /gpfs/scratch/bsc88/bsc88251/PubMedTranslate/all_sentences.src
+bash tokenize_SP.sh -d /gpfs/projects/bsc88/_old/Resources/NMT_WMT19 -s en -t es -f /gpfs/home/bsc88/bsc88251/TranslateADRLexicon/data/ADR_lexicon_terms.txt
 
 source /gpfs/projects/bsc88/Environments_P9/OpenNMT-tf/bin/activate
 
 
 python3 /gpfs/projects/bsc88/_old/ExternalTools/OpenNMT-py/translate.py -gpu 0 -model /gpfs/projects/bsc88/_old/Resources/NMT_WMT19/pten_es_model.pt \
--src /gpfs/scratch/bsc88/bsc88251/PubMedTranslate/all_sentences.src.tok.part.2 -replace_unk \
--output /gpfs/scratch/bsc88/bsc88251/PubMedTranslate/all_sentences.src.translated.part.2 -batch_size 60
+-src /gpfs/home/bsc88/bsc88251/TranslateADRLexicon/data/ADR_lexicon_terms.txt.tok -replace_unk \
+-output /gpfs/home/bsc88/bsc88251/TranslateADRLexicon/data/ADR_lexicon_terms.txt.tok.translated -batch_size 60
